723f2c7bc8b9c337fc6585816c11f2e583562418
SheYan <shelley.yang@softwire.com>
2025-07-03 15:56:11 +0100

    Revert "Added a test that rooks can take pieces"
    This reverts commit 959dd06780e12626f0b838c58daebe28de46c7cd.
    
    r

--- tests/engine/pieces/rook.spec.ts
+++ tests/engine/pieces/rook.spec.ts
@@ -3,7 +3,6 @@ import Board from '../../../src/engine/board';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
 import Pawn from '../../../src/engine/pieces/pawn';
-import King from '../../../src/engine/pieces/king';
 
 describe('Rook', () => {
     let board: Board;
@@ -54,38 +53,5 @@ describe('Rook', () => {
         const moves = rook.getAvailableMoves(board);
 
         moves.should.not.deep.include(Square.at(4, 7));
-    });
-
-    it('can take opposing pieces', () => {
-        const rook = new Rook(Player.WHITE);
-        const opposingPiece = new Pawn(Player.BLACK);
-        board.setPiece(Square.at(4, 4), rook);
-        board.setPiece(Square.at(4, 6), opposingPiece);
-
-        const moves = rook.getAvailableMoves(board);
-
-        moves.should.deep.include(Square.at(4, 6));
-    });
-
-    it('cannot take the opposing king', () => {
-        const rook = new Rook(Player.WHITE);
-        const opposingKing = new King(Player.BLACK);
-        board.setPiece(Square.at(4, 4), rook);
-        board.setPiece(Square.at(4, 6), opposingKing);
-
-        const moves = rook.getAvailableMoves(board);
-
-        moves.should.not.deep.include(Square.at(4, 6));
-    });
-
-    it('cannot take friendly pieces', () => {
-        const rook = new Rook(Player.WHITE);
-        const friendlyPiece = new Pawn(Player.WHITE);
-        board.setPiece(Square.at(4, 4), rook);
-        board.setPiece(Square.at(4, 6), friendlyPiece);
-
-        const moves = rook.getAvailableMoves(board);
-
-        moves.should.not.deep.include(Square.at(4, 6));
     });
 });
