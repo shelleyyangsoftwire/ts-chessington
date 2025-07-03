aa121b9211cce040a8f457081aef7f3f3efc8116
SheYan <shelley.yang@softwire.com>
2025-07-03 10:16:30 +0100

    Possible moves for pawns -- Red 1

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,18 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        // get the square this pawn is on
+        let location = board.findPiece(this);
+        // return the square in front of it
+        var possibleMoves = [];
+        var thisMove = {};
+        if (this.player == Player.WHITE){
+            thisMove = new Square(location.row + 1, location.col);
+        } else if (this.player == Player.BLACK) {
+            thisMove = new Square(location.row - 1, location.col);
+        }
+        possibleMoves.push(thisMove);
+        return possibleMoves;
     }
 }
+
