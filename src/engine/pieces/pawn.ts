d753881c66a8b1b2d6b7821740f0a8e452a15b7c
SheYan <shelley.yang@softwire.com>
2025-07-03 11:30:07 +0100

    pawn moves -- first move

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -12,14 +12,19 @@ export default class Pawn extends Piece {
         // get the square this pawn is on
         let location = board.findPiece(this);
         // return the square in front of it
-        var possibleMoves = [];
-        var thisMove = {};
+        let possibleMoves = [];
         if (this.player == Player.WHITE){
-            thisMove = new Square(location.row + 1, location.col);
+            possibleMoves.push( new Square(location.row + 1, location.col));
+            if (location.row == 1){
+                possibleMoves.push( new Square(location.row + 2, location.col));
+            }
         } else if (this.player == Player.BLACK) {
-            thisMove = new Square(location.row - 1, location.col);
+            possibleMoves.push( new Square(location.row - 1, location.col));
+            if (location.row == 6){
+                possibleMoves.push( new Square(location.row - 2, location.col));
+            }
         }
-        possibleMoves.push(thisMove);
+
         return possibleMoves;
     }
 }
