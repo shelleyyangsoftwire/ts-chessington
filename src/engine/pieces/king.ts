31e4ea9df9c2d2af37443ce65299b4fbef0119ac
SheYan <shelley.yang@softwire.com>
2025-07-03 14:06:03 +0100

    king via brute force -- kept trying to loop and it never quite worked

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,43 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let possibleMoves = [];
+        let i = 1;
+        //diagonal moves
+        if (location.row + i < 8 && location.col +i < 8){
+            possibleMoves.push(new Square (location.row + i, location.col + i));
+        }
+        if (location.row + i < 8 && location.col - i >= 0){
+            possibleMoves.push(new Square (location.row + i, location.col - i));
+        }
+        if (location.row - i >= 0 && location.col +i < 8){
+            possibleMoves.push(new Square (location.row - i, location.col + i));
+        }
+        if (location.row - i >= 0 && location.col - i >= 0){
+            possibleMoves.push(new Square (location.row - i, location.col - i));
+        }
+
+        if (location.row + i < 8) {
+            possibleMoves.push(new Square(location.row + i, location.col));
+        }
+        if (location.row - i >= 0 ) {
+            possibleMoves.push(new Square(location.row - i, location.col));
+        }
+        if (location.col + i < 8) {
+            possibleMoves.push(new Square(location.row, location.col + i));
+        }
+        if (location.col - i >= 0 ) {
+            possibleMoves.push(new Square(location.row, location.col - i));
+        }
+
+
+
+       // if (location.row + 1 < 8 && location.col +1 < 8){
+         //   possibleMoves.push(new Square (location.row + i, location.col + i));
+        //}
+
+
+        return possibleMoves;
     }
 }
