950a167e2a6b88e2d074c9736225fbec570dd1b4
SheYan <shelley.yang@softwire.com>
2025-07-03 16:44:09 +0100

    queen eating (and left no crumbs)

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from "../square";
+import King from "./king";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -12,85 +13,143 @@ export default class Queen extends Piece {
         let location = board.findPiece(this);
         // return the square in front of it
         let possibleMoves = [];
-
+        let player = this.player;
         var row = location.row;
         var col = location.col;
-        function ifEmpty(x: number, y:number){
+
+        function inBounds (x: number, y: number){
             if (x < 0 || x > 7 || y < 0 || y > 7){
                 return false;
+            } else {
+                return true;
             }
+        }
+        function ifEmpty(x: number, y:number){
+
             var thisSquare = new Square(x, y);
             if (board.getPiece(thisSquare) == null){
                 return true;
             }
             return false;
         }
-        // diagonal moves
+
+        function ifEdible (x: number, y: number, me: number ){
+            var thisSquare = new Square(x, y);
+            var contents = board.getPiece(thisSquare);
+            if (contents instanceof Piece && contents.player != me){
+                if (contents instanceof King){
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        //moving in all directions
+
+        // right
+        for (var i = 1; i < 8; i ++){
+            if (inBounds(row, col + i)){
+                if (ifEmpty(row, col + i)){
+                    possibleMoves.push(new Square(row, col + i));
+                } else {
+                    if (ifEdible(row, col + i, player)){
+                        possibleMoves.push(new Square(row, col + i));
+                    }
+                    break;
+                }
+            }
+        }
+
+
+        // left
+        for (var i = 1; i < 8; i ++){
+            if (inBounds(row, col - i)) {
+                if (ifEmpty(row, col - i)) {
+                    possibleMoves.push(new Square(row, col - i));
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // up
+        for (var i = 1; i < 8; i ++){
+            if (inBounds(row+ i, col)) {
+                if (ifEmpty(row + i, col)) {
+                    possibleMoves.push(new Square(row + i, col));
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // down
+        for (var i = 1; i < 8; i ++){
+            if (inBounds(row- i, col)) {
+                if (ifEmpty(row - i, col)) {
+                    possibleMoves.push(new Square(row - i, col));
+                } else {
+                    break;
+                }
+            }
+        }
+
         for (var i = 1; i < 8; i++){
+            if (! inBounds(row + i, col + i)){
+                break;
+            }
             if (ifEmpty(row + i, col + i)){
                 possibleMoves.push(new Square (row + i, col + i));
             } else {
+                if (ifEdible(row + i, col + i, player)){
+                    possibleMoves.push(new Square (row + i, col + i));
+                }
                 break;
             }
         }
 
         for (var i = 1; i < 8; i++){
+            if (! inBounds(row + i, col - i)){
+                break;
+            }
             if (ifEmpty(row + i, col - i)){
                 possibleMoves.push(new Square (row + i, col - i));
             } else {
+                if (ifEdible(row + i, col - i, player)){
+                    possibleMoves.push(new Square (row + i, col - i));
+                }
                 break;
             }
         }
 
         for (var i = 1; i < 8; i++){
+            if (! inBounds(row - i, col + i)){
+                break;
+            }
             if (ifEmpty(row - i, col + i)){
                 possibleMoves.push(new Square (row - i, col + i));
             } else {
+                if (ifEdible(row - i, col + i, player)){
+                    possibleMoves.push(new Square (row - i, col + i));
+                }
                 break;
             }
         }
 
         for (var i = 1; i < 8; i++){
+            if (! inBounds(row - i, col - i)){
+                break;
+            }
             if (ifEmpty(row - i, col - i)){
                 possibleMoves.push(new Square (row - i, col - i));
             } else {
+                if (ifEdible(row - i, col - i, player)){
+                    possibleMoves.push(new Square (row - i, col - i));
+                }
                 break;
             }
         }
-
-        //lateral moves
-        for (var i = 1; i < 8; i ++){
-            if (ifEmpty(row, col + i)){
-                possibleMoves.push(new Square(row, col + i));
-            } else {
-                break;
-            }
-        }
-
-        for (var i = 1; i < 8; i ++){
-            if (ifEmpty(row, col - i)){
-                possibleMoves.push(new Square(row, col - i));
-            } else {
-                break;
-            }
-        }
-
-        for (var i = 1; i < 8; i ++){
-            if (ifEmpty(row + i, col)){
-                possibleMoves.push(new Square(row + i, col));
-            } else {
-                break;
-            }
-        }
-
-        for (var i = 1; i < 8; i ++){
-            if (ifEmpty(row - i, col)){
-                possibleMoves.push(new Square(row - i, col));
-            } else {
-                break;
-            }
-        }
-
         return possibleMoves;
     }
 }
