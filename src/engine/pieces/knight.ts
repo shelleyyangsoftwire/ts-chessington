61c323c6f0f90a5a2012b4b2830ad396fca440f5
SheYan <shelley.yang@softwire.com>
2025-07-03 12:11:59 +0100

    knight moves

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,31 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let row = location.row;
+        let col = location.col;
+        // return the square in front of it
+        let possibleMoves = [];
+        for (var i = 1; i <=2; i++){
+            for (var j = 1; j <=2; j++){
+                if (i != j){
+                    let thisSet = [];
+                    if (row + i < 8 && col + j < 8){
+                        possibleMoves.push(new Square (row + i, col + j));
+                    }
+                    if (row + i < 8 && col - j >= 0){
+                        possibleMoves.push(new Square (row + i, col - j));
+                    }
+                    if (row - i >= 0 && col + j < 8){
+                        possibleMoves.push(new Square (row - i, col + j));
+                    }
+                    if (row - i >= 0 && col - j >= 0){
+                        possibleMoves.push(new Square (row - i, col - j));
+                    }
+                }
+            }
+        }
+
+        return possibleMoves;
     }
 }
